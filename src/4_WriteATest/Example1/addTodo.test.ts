a8d432b7383edaa2cd16981e2bc4d67ebdcf6b58
Katie Ng <katie.jane.ng@gmail.com>
2024-02-13 11:50:47 +1100

    add solutions

--- src/4_WriteATest/Example1/addTodo.test.ts
+++ src/4_WriteATest/Example1/addTodo.test.ts
@@ -1,5 +1,31 @@
-describe("Name of the group", () => {
-  it("should do something", () => {
-    expect(true).toBe(true);
+import { addTodo1 } from "./addTodo";
+import { randomUUID } from "crypto";
+jest.mock("crypto");
+
+const mockRandomUUID = jest.mocked(randomUUID);
+
+describe("addTodo1", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should add item to empty todo list", () => {
+    // ARRANGE
+    const fakeUUID = "25EDC755-5DD2-479B-A48F-29866D639BB9";
+    mockRandomUUID.mockReturnValueOnce(fakeUUID);
+
+    const newTodo = { text: "Feed the dog" };
+
+    // ACT
+    const result = addTodo1([], newTodo);
+
+    // ASSERT
+    expect(result).toEqual<Todo[]>([
+      {
+        id: fakeUUID,
+        text: "Feed the dog",
+        completed: false,
+      },
+    ]);
   });
 });
--- src/4_WriteATest/Example1/addTodo.ts
+++ src/4_WriteATest/Example1/addTodo.ts
@@ -1,10 +1,19 @@
+import { randomUUID } from "crypto";
 interface Todo {
   id: string;
   text: string;
   completed: boolean;
 }
-let todoList1: Todo[] = [];
 
-const addTodo1 = (newTodo: { text: string }): Todo[] => {
-  return [];
+export const addTodo1 = (
+  currentTodos: Todo[],
+  newTodo: { text: string }
+): Todo[] => {
+  const todo = {
+    text: newTodo.text,
+    completed: false,
+    id: randomUUID(),
+  };
+
+  return [...currentTodos, todo];
 };
